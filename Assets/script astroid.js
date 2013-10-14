d67324896ef4d12c0339ef67a454e2f02132a7e4
liusashmily <liusashmily@gmail.com>
2013-10-14 18:15:55 -0500

    asteroid script

--- Assets/script astroid.js
+++ Assets/script astroid.js
@@ -1,9 +1,20 @@
 ﻿#pragma strict
 
+// Inspector Variable
+var astroidSpeed :float = 6.0;
+
 function Start () {
 
 }
 
 function Update () {
+//	var newSpeed = astroidSpeed * Time.timedeltaTime;
+	transform.Translate(Vector3.down * astroidSpeed * Time.deltaTime);
+
+	if (transform.position.y <= -20){
+		transform.position.y = 15;
+		transform.position.x = Random.Range (-20.0, 20.0);
+	}
+
 
 }
