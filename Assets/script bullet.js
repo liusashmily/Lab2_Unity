b68df77733d4913eeca1ab29304c1ad3da2ccc7f
liusashmily <liusashmily@gmail.com>
2013-10-13 22:18:30 -0500

    create the player bullet

--- Assets/script bullet.js
+++ Assets/script bullet.js
@@ -1,9 +1,16 @@
 ﻿#pragma strict
 
+//Inspector Variable
+var bulletSpeed: float = 15.0;			// Speed of bullet
+
+
 function Start () {
 
 }
 
+// Game Loop
 function Update () {
-
+	transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+	if (transform.position.y >= 10)
+		Destroy (gameObject);
 }
