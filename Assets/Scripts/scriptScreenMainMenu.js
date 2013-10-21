bdacb93c33dfec856c16872bb0dee42eacdf9a99
liusashmily <liusashmily@gmail.com>
2013-10-21 16:12:51 -0500

    5 20 screen main

--- Assets/Scripts/scriptScreenMainMenu.js
+++ Assets/Scripts/scriptScreenMainMenu.js
@@ -1,9 +1,33 @@
 ﻿#pragma strict
 
-function Start () {
+// Main Menu Script
 
-}
+function OnGUI () {
+	// Make a group on the center of the screen
+	GUI.BeginGroup (Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 175));
 
-function Update () {
+	// Make a box to see the group on screen
+	GUI.Box (Rect(0,0,100,175), "Main Menu");
 
+	// Add buttons for game navigation
+	if(GUI.Button(Rect(10,30,80,30), "Start Game")){
+		Application.LoadLevel("ScreenLoad");
+	}
+
+	// Add buttons for game navigation
+	if(GUI.Button(Rect(10,60,80,30), "Credits")){
+		Application.LoadLevel("ScreenCredit");
+	}
+
+	// Add buttons for game navigation
+	if(GUI.Button(Rect(10,90,80,30), "Exit")){
+		Application.Quit();
+	}
+
+	// Add buttons for game navigation
+	if(GUI.Button(Rect(10,120,80,30), "Home Page")){
+		Application.OpenURL("http://www.lisaliu.info");
+	}
+
+	GUI.EndGroup ();
 }
